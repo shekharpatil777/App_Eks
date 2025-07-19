779818c8de5cfd2c1b2f6c257ff85555305f07db
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-07-19 21:30:33 +0530

    Update log_level.py

--- python/log_level.py
+++ python/log_level.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-
+#different type of log levels
 logging.debug("Debug message")
 logging.info("Info message")
 logging.warning("Warning message")
