bc8fb15cdb7980f49a93e745492b044014179eec
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-07-23 23:38:23 +0530

    Create Rotate_file_handler.py

---
+++ python/Rotate_file_handler.py
@@ -0,0 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+handler = RotatingFileHandler('rotate.log', maxBytes=1000, backupCount=3)
+logging.basicConfig(handlers=[handler], level=logging.INFO)
+logging.info("This will rotate when the log file reaches 1KB.")
