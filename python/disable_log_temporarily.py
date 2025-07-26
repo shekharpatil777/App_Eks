f18bd115319693abce66a6c75af489850d39fbbf
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-07-26 19:19:39 +0530

    Update disable_log_temporarily.py

--- python/disable_log_temporarily.py
+++ python/disable_log_temporarily.py
@@ -1,5 +1,6 @@
 import logging
 
+#Disable Logging Temporarily
 logging.disable(logging.CRITICAL)  # Disable all logging
 logging.warning("This won't show")
 logging.disable(logging.NOTSET)    # Enable logging again
