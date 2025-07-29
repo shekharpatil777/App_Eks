8f3e0c2c615c110bfb0a89b7a6aacea62d5eca9a
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-07-29 23:05:05 +0530

    Create decorators/basic_decorator.py

---
+++ python/decorators/basic_decorator.py
@@ -0,0 +1,12 @@
+def my_decorator(func):
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+
+@my_decorator
+def greet():
+    print("Hello!")
+
+greet()
