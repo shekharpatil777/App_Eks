66123b61813abb4140db66066879d3f111b39d62
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-07-31 23:07:45 +0530

    Create logging_decorator.py

---
+++ python/decorators/logging_decorator.py
@@ -0,0 +1,13 @@
+def logger(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with args={args} kwargs={kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned {result}")
+        return result
+    return wrapper
+
+@logger
+def add(a, b):
+    return a + b
+
+add(2, 3)
