43ce19ecd3dd97a21861f495aaae9c9f7aa63e66
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-08-09 23:19:23 +0530

    Create multiple_thread.py

---
+++ python/multithreading/multiple_thread.py
@@ -0,0 +1,13 @@
+import threading
+
+def worker(num):
+    print(f"Worker {num} is working")
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=worker, args=(i,))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
