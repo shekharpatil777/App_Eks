c71126201c46587547406e7bb0ef1e759fdb4459
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-08-17 21:59:12 +0530

    Update sum_array.py

--- python/sum_array.py
+++ python/sum_array.py
@@ -1,5 +1,7 @@
 def twoSum_brute_force(nums, target):
     n = len(nums)
     for i in range(n):
-
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
     return [] # Should not be reached based on the problem statement
