237932c74f6b5ce9e98fa2e618c1de4603a73f97
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-08-21 19:20:20 +0530

    Create stack.py

---
+++ python/Data_structure/stack.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return "Stack is empty"
+
+    def size(self):
+        return len(self.items)
