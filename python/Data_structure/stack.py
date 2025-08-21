2d04956574f52ef8ef88c2c3a890fb4b22fbb5f2
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-08-21 19:20:32 +0530

    Update stack.py

--- python/Data_structure/stack.py
+++ python/Data_structure/stack.py
@@ -8,7 +8,11 @@ class Stack:
     def push(self, item):
         self.items.append(item)
 
-
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            return "Stack is empty"
 
     def peek(self):
         if not self.is_empty():
