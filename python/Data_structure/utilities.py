062fe1fadc142840f4b4d33e6c95288293acdbb6
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-01 23:08:50 +0530

    Update utilities.py

--- python/Data_structure/utilities.py
+++ python/Data_structure/utilities.py
@@ -9,4 +9,10 @@ print(list(it.accumulate([1, 2, 3, 4], operator.add)))
 print(list(it.chain("ABC", "XYZ")))
 # ['A', 'B', 'C', 'X', 'Y', 'Z']
 
+# islice(iterable, start, stop, step)
+print(list(it.islice(range(10), 2, 8, 2)))
+# [2, 4, 6]
 
+# starmap(function, iterable)
+print(list(it.starmap(pow, [(2, 5), (3, 2), (10, 3)])))
+# [32, 9, 1000]
