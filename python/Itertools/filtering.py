180974752cf5e465abdecad8bc5581476949ad60
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-02 23:34:44 +0530

    Update filtering.py

--- python/Itertools/filtering.py
+++ python/Itertools/filtering.py
@@ -8,4 +8,11 @@ print(list(it.compress('ABCDEF', [1, 0, 1, 0, 1, 1])))
 print(list(it.dropwhile(lambda x: x < 5, [1, 4, 6, 4, 7])))
 # [6, 4, 7]
 
+# takewhile(predicate, iterable)
+print(list(it.takewhile(lambda x: x < 5, [1, 4, 6, 4, 7])))
+# [1, 4]
+
+# filterfalse(predicate, iterable)
+print(list(it.filterfalse(lambda x: x % 2, range(10))))
+# [0, 2, 4, 6, 8]
 
