d4e4e8ba5cfb81a97549bfc3ca78bdebf09a99aa
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-08 23:29:26 +0530

    Update sets.py

--- python/Data_structure/sets.py
+++ python/Data_structure/sets.py
@@ -2,4 +2,12 @@
 nums = [1, 2, 3, 1, 4]
 print(len(nums) != len(set(nums)))  # True
 
+# Two sum problem
+nums = [2, 7, 11, 15]
+target = 9
+seen = {}
+for i, num in enumerate(nums):
+    if target - num in seen:
+        print((seen[target-num], i))  # (0, 1)
+    seen[num] = i
 
