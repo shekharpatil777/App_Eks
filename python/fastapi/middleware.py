aef9a324bd29d7d6a8dbc017096109698dad8501
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-11 22:49:24 +0530

    Update middleware.py

--- python/fastapi/middleware.py
+++ python/fastapi/middleware.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-
+@app.middleware("http")
 async def log_requests(request: Request, call_next):
     print(f"Incoming request: {request.url}")
     response = await call_next(request)
