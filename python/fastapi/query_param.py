71d83406b1f33ed97c71b1704b55ca48beb0cf51
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-12 22:28:32 +0530

    Update query_param.py

--- python/fastapi/query_param.py
+++ python/fastapi/query_param.py
@@ -3,6 +3,6 @@ from typing import Union
 
 app = FastAPI()
 
-
+@app.get("/items/")
 async def read_items(skip: int = 0, limit: int = 10, q: Union[str, None] = None):
     return {"skip": skip, "limit": limit, "q": q}
