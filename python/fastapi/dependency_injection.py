1f4300b5b068b698e336600403d7eedbbd124a9a
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-14 22:23:12 +0530

    Update and rename dependcy_injection.py to dependency_injection.py

--- python/fastapi/dependency_injection.py
+++ python/fastapi/dependency_injection.py
@@ -3,3 +3,7 @@ from fastapi import Depends
 def common_params(q: str | None = None, limit: int = 10):
     return {"q": q, "limit": limit}
 
+@app.get("/search/")
+def search(params: dict = Depends(common_params)):
+    return params
+
