fe52a1b555750eff6e9dbf97cd8a613ffbeca801
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-16 22:46:36 +0530

    Update response_model.py

--- python/fastapi/response_model.py
+++ python/fastapi/response_model.py
@@ -2,3 +2,7 @@ class User(BaseModel):
     username: str
     email: str
 
+
+@app.get("/user/{username}", response_model=User)
+def get_user(username: str):
+    return {"username": username, "email": f"{username}@example.com"}
