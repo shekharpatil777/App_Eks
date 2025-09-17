bd48fd8c8cb98a16337e443a1d5950a5f3f6fb2c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-17 21:48:54 +0530

    Update exception_handling.py

--- python/fastapi/exception_handling.py
+++ python/fastapi/exception_handling.py
@@ -8,3 +8,8 @@ class CustomException(Exception):
 @app.exception_handler(CustomException)
 def custom_handler(request: Request, exc: CustomException):
     return JSONResponse(status_code=418, content={"message": f"Oops! {exc.name}"})
+
+
+@app.get("/error/")
+def error():
+    raise CustomException("Something went wrong")
