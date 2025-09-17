08223ebaf76aad775e2bd5f60cac2ce19c66953b
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-17 21:48:39 +0530

    Create exception_handling.py

---
+++ python/fastapi/exception_handling.py
@@ -0,0 +1,10 @@
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
+
+class CustomException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+@app.exception_handler(CustomException)
+def custom_handler(request: Request, exc: CustomException):
+    return JSONResponse(status_code=418, content={"message": f"Oops! {exc.name}"})
