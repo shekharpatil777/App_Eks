e9332dc7936b8e2e986cd109501dc8495818ff21
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-09-25 23:36:57 +0530

    Update websocket.py

--- python/fastapi/websocket.py
+++ python/fastapi/websocket.py
@@ -19,3 +19,14 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    await manager.broadcast(f"Client #{client_id} has joined the chat.")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} has left the chat.")
