0fa181b72daed45d6c7e64438a568e454727721d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-10-13 22:11:10 +0530

    Update int_palindrome.py

--- python/leetcode/int_palindrome.py
+++ python/leetcode/int_palindrome.py
@@ -7,6 +7,9 @@ class Solution:
         original = x
         reversed_num = 0
 
-
+        while x > 0:
+            digit = x % 10
+            reversed_num = reversed_num * 10 + digit
+            x //= 10
 
         return original == reversed_num
