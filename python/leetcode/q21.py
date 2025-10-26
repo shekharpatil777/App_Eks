20f12acfdddb06804fcdb8eadf0025b255a2423f
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-10-26 20:55:48 +0530

    Update q21.py

--- python/leetcode/q21.py
+++ python/leetcode/q21.py
@@ -9,7 +9,14 @@ class Solution:
         dummy = ListNode()  # Dummy node to start the merged list
         tail = dummy
 
-
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
 
         # Attach the remaining part of the list
         tail.next = list1 if list1 else list2
