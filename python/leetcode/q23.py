d6a2e3d4e9fcbfde6c134acd163a9a7cd0324d16
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-10-28 22:07:20 +0530

    Create q23.py

---
+++ python/leetcode/q23.py
@@ -0,0 +1,8 @@
+from heapq import heappush, heappop
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
