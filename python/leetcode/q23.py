bee94a5036761ce550c6ba416777789cdb8f2d60
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-10-28 22:07:31 +0530

    Update q23.py

--- python/leetcode/q23.py
+++ python/leetcode/q23.py
@@ -6,3 +6,22 @@ class ListNode:
         self.val = val
         self.next = next
 
+class Solution:
+    def mergeKLists(self, lists):
+        heap = []
+        # Push head of all lists into heap
+        for i, node in enumerate(lists):
+            if node:
+                heappush(heap, (node.val, i, node))
+        
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while heap:
+            val, i, node = heappop(heap)
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heappush(heap, (node.next.val, i, node.next))
+        
+        return dummy.next
