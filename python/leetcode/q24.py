9f140e04a4492de1a2134a860d1211a7fb14c389
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-10-29 22:18:56 +0530

    Update q24.py

--- python/leetcode/q24.py
+++ python/leetcode/q24.py
@@ -8,5 +8,18 @@ def swapPairs(head: ListNode) -> ListNode:
     dummy = ListNode(0)
     dummy.next = head
     prev = dummy
+    # prev -> first -> second -> following
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = first.next
 
+        # swap
+        prev.next = second
+        first.next = second.next
+        second.next = first
+
+        # move prev two nodes forward
+        prev = first
+
+    return dummy.next
 
