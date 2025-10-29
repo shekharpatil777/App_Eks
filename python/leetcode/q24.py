d83389fb4c63efe8618f98301796f1517920d9b0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-10-29 22:18:47 +0530

    Create q24.py

---
+++ python/leetcode/q24.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def swapPairs(head: ListNode) -> ListNode:
+    dummy = ListNode(0)
+    dummy.next = head
+    prev = dummy
+
+
