168f33468dc89d444f2dbbda44e0d846d02c5f2d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-11-15 23:36:18 +0530

    Create q38.py

---
+++ python/leetcode/q38.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        
+        s = "1"
+        for _ in range(n - 1):
+            result = []
+            i = 0
+            while i < len(s):
+                count = 1
+                while i + 1 < len(s) and s[i] == s[i + 1]:
