bf139c1b5083b7eb338fd37236c8da31408b399d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-11-15 23:36:31 +0530

    Update q38.py

--- python/leetcode/q38.py
+++ python/leetcode/q38.py
@@ -10,3 +10,10 @@ class Solution:
             while i < len(s):
                 count = 1
                 while i + 1 < len(s) and s[i] == s[i + 1]:
+                    count += 1
+                    i += 1
+                result.append(str(count) + s[i])
+                i += 1
+            s = "".join(result)
+        
+        return s
