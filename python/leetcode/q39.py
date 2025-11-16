01c32167f0e035af12d6bd0fe481ee33f95b8bda
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-11-16 22:31:57 +0530

    Update q39.py

--- python/leetcode/q39.py
+++ python/leetcode/q39.py
@@ -15,4 +15,9 @@ class Solution:
             # Explore candidates from 'start' index to avoid duplicates
             for i in range(start, len(candidates)):
                 
+curr_list.append(candidates[i])
+                backtrack(i, curr_list, curr_sum + candidates[i])  # i (not i+1) because reuse allowed
+                curr_list.pop()  # backtrack
 
+        backtrack(0, [], 0)
+        return res
