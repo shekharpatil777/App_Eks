9c14f513a407fb83110af098df4b2979299294e0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-11-17 23:06:44 +0530

    Create q40.py

---
+++ python/leetcode/q40.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        res = []
+
+        def backtrack(start, path, remaining):
+            if remaining == 0:
+                res.append(path[:])
+                return
+
+            for i in range(start, len(candidates)):
+                # Skip duplicates
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                # If number exceeds remaining target, stop
+                if candidates[i] > remaining:
+                    break
+
+
