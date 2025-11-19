0fbfdbb2b46598da800bd0d26dc873341f74d1a3
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-11-19 23:16:22 +0530

    Create q42.py

---
+++ python/leetcode/q42.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height):
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    water += left_max - height[left]
+                left += 1
+            else:
+ 
+
+        return water
