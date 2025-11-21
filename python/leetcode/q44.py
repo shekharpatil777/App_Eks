744a79b6493091651236f3358a921a7438b78094
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-11-21 21:49:58 +0530

    Create q44.py

---
+++ python/leetcode/q44.py
@@ -0,0 +1,18 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        m, n = len(num1), len(num2)
+        result = [0] * (m + n)
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = (ord(num1[i]) - 48) * (ord(num2[j]) - 48)
+                sum_ = mul + result[i + j + 1]
+
+                result[i + j + 1] = sum_ % 10
+                result[i + j] += sum_ // 10
+
+  
+
