28dc63656f55ebb3c00de965aafa335f6d923f05
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-12-03 19:17:35 +0530

    Create q50.py

---
+++ python/leetcode/q50.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Handle negative exponent
+        if n < 0:
+            x = 1 / x
+            N = -n  # Use a temporary variable for the exponent
+        else:
+            N = n
+
+        ans = 1.0
+        
+        # Binary Exponentiation (Iterative)
+        while N > 0:
+            # If the current bit is 1 (N is odd), include the current power of x in the result
+            if N % 2 == 1:
+                ans = ans * x
+            
+            # Square the base for the next iteration
+            x = x * x
+            
+            # Move to the next bit (integer division by 2)
+            N = N // 2
+            
+        return ans
+
