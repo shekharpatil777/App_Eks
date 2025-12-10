8da9237c7e428351a55965730fdab830d0cb37a5
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-12-10 23:31:15 +0530

    Create q57.py

---
+++ python/leetcode/q57.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        ans = []
+        i = 0
+
+        # 1. Add all non-overlapping intervals that end before newInterval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            ans.append(intervals[i])
+            i += 1
+
+        # 2. Merge all overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        # Add the merged newInterval to the result list
+        ans.append(newInterval)
+
