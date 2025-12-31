0fa70c25df9c640eeb147e21d0a19fe14b6673a6
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-12-31 21:44:33 +0530

    Create q75.py

---
+++ python/leetcode/q75.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
