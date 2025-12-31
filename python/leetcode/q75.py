170aa040e287eb3e070918b2ddd4df7e26927d43
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-12-31 21:44:51 +0530

    Update q75.py

--- python/leetcode/q75.py
+++ python/leetcode/q75.py
@@ -7,3 +7,18 @@ class Solution:
         mid = 0
         high = len(nums) - 1
 
+        while mid <= high:
+            if nums[mid] == 0:
+                # Swap mid with low to move the 0 to the front
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                # 1 is already in the "middle" zone, just move forward
+                mid += 1
+            else: # nums[mid] == 2
+                # Swap mid with high to move the 2 to the end
+                nums[mid], nums[high] = nums[high], nums[mid]
+                # Decrement high, but DON'T increment mid yet 
+                # because the new nums[mid] needs to be checked.
+                high -= 1
