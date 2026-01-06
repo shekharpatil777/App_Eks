d5c075151ab340419ecd716e5e0d1541a5d640ee
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-06 22:58:25 +0530

    Update q80.py

--- python/leetcode/q80.py
+++ python/leetcode/q80.py
@@ -8,6 +8,9 @@ class Solution:
         for x in nums:
             # We add the element if:
             # 1. We have added fewer than 2 elements total (k < 2)
-
+            # 2. The current element is different from the one 2 spots back 
+            if k < 2 or x != nums[k - 2]:
+                nums[k] = x
+                k += 1
                 
         return k
