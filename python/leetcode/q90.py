4f4e50700631deb63d9c9ca4e3e2c61c6bc3652c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-18 17:50:44 +0530

    Update q90.py

--- python/leetcode/q90.py
+++ python/leetcode/q90.py
@@ -10,3 +10,13 @@ class Solution:
             for i in range(start, len(nums)):
                 # Step 2: Skip duplicates
                 # If nums[i] is same as nums[i-1], and we aren't at the 'start' index,
+                # it means we've already processed this value in this position.
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop() # Backtrack
+                
+        backtrack(0, [])
+        return res
