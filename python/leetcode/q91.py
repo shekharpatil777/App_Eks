bab2d9b81632c5d3b273df8bd1bf9f853527d66c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-20 22:29:53 +0530

    Update q91.py

--- python/leetcode/q91.py
+++ python/leetcode/q91.py
@@ -11,3 +11,14 @@ class Solution:
         dp[0] = 1 # An empty string has 1 way to be decoded (doing nothing)
         dp[1] = 1 # We already checked that s[0] != '0'
         
+        for i in range(2, n + 1):
+            # Check single digit: s[i-1]
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            
+            # Check two digits: s[i-2:i]
+            two_digit = int(s[i-2:i])
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i-2]
+                
+        return dp[n]
