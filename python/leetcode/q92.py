00eff1b060be8a2b553813b189fe8f5d74733b5c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-21 22:59:00 +0530

    Create q92.py

---
+++ python/leetcode/q92.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        # Move prev to node before 'left'
+        for _ in range(left - 1):
+            prev = prev.next
+
+
