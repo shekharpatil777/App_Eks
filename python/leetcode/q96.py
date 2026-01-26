0c27c11a263768b5680fe6c9bcb56821a33e12d5
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-26 22:42:06 +0530

    Update q96.py

--- python/leetcode/q96.py
+++ python/leetcode/q96.py
@@ -8,3 +8,11 @@ class Solution:
         # 1 node -> 1 tree
         dp[0], dp[1] = 1, 1
         
+        # Fill the DP table for each number of nodes from 2 up to n
+        for nodes in range(2, n + 1):
+            for root in range(1, nodes + 1):
+                # left nodes = root - 1
+                # right nodes = nodes - root
+                dp[nodes] += dp[root - 1] * dp[nodes - root]
+                
+        return dp[n]
