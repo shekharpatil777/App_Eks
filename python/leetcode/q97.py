15fb2a5493e2b3893b1bb147b8b3b6663b5a28c0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-27 22:34:42 +0530

    Update q97.py

--- python/leetcode/q97.py
+++ python/leetcode/q97.py
@@ -4,3 +4,10 @@ class Solution:
         dp = [0] * (n + 1)
         dp[0], dp[1] = 1, 1
         
+        # Build up from 2 nodes to n nodes
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                # j is the root: left side has j-1 nodes, right side has i-j nodes
+                dp[i] += dp[j - 1] * dp[i - j]
+        
+        return dp[n]
