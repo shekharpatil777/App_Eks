fa5c031fc71ea95f9a73c9e12ef38be243804b7c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-29 22:34:27 +0530

    Create q99.py

---
+++ python/leetcode/q99.py
@@ -0,0 +1,23 @@
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        self.first = self.second = self.prev = None
+        
+        def inorder(node):
+            if not node:
+                return
+            
+            inorder(node.left)
+            
+            # Detect the swapped nodes
+            if self.prev and node.val < self.prev.val:
+                # If first anomaly is found, 'prev' is the suspect
+                if not self.first:
+                    self.first = self.prev
+                # 'node' is always the suspect for the second anomaly
+                self.second = node
+            
+            self.prev = node
+            
+            inorder(node.right)
+        
+        inorder(root)
