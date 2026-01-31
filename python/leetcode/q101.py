1894bbc7d436757792fdd4b436ca23c1ec226a1d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-01-31 22:41:55 +0530

    Update q101.py

--- python/leetcode/q101.py
+++ python/leetcode/q101.py
@@ -15,3 +15,8 @@ class Solution:
             if not t1 or not t2 or t1.val != t2.val:
                 return False
             
+            # Add pairs in mirroring order
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+            
+        return True
