a81e5f586445ae8b5e00b5baefa820f2d761617c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-02 23:30:02 +0530

    Create q103.py

---
+++ python/leetcode/q103.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        results = []
+        queue = deque([root])
+        left_to_right = True
+        
+        while queue:
+            level_size = len(queue)
+            current_level = deque()
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                
+                # If left_to_right, add to the end; else, add to the front
+                if left_to_right:
+                    current_level.append(node.val)
+                else:
+                    current_level.appendleft(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            
