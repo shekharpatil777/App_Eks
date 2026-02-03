ac8ee85c594bfb4f33d6dc8d06734cce40e6647d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-03 23:08:08 +0530

    Update q104.py

--- python/leetcode/q104.py
+++ python/leetcode/q104.py
@@ -11,4 +11,5 @@ class Solution:
         if not root:
             return 0
         
-       
+ #Recursive Case: 1 (current node) + the max of its children
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))      
