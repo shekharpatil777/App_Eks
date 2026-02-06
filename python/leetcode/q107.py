09881d83f94c49662bc798afb6dae5b8e756d3d5
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-06 23:28:15 +0530

    Create q107.py

---
+++ python/leetcode/q107.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrderBottom(self, root):
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                
+
