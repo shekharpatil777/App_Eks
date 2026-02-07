77d5ee4cb920fa6103dd2f9f9f83f647ad287c48
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-07 22:04:40 +0530

    Update q108.py

--- python/leetcode/q108.py
+++ python/leetcode/q108.py
@@ -11,3 +11,10 @@ class Solution:
             if left > right:
                 return None
 
+            mid = (left + right) // 2
+            root = TreeNode(nums[mid])
+            root.left = build(left, mid - 1)
+            root.right = build(mid + 1, right)
+            return root
+
+        return build(0, len(nums) - 1)
