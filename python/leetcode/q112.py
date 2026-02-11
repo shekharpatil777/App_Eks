7838a0d9b2b1afa79a1baeebeb18645bffcb8f16
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-11 23:03:35 +0530

    Create q112.py

---
+++ python/leetcode/q112.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        # If it's a leaf node
+        if not root.left and not root.right:
+            return targetSum == root.val
+        
+
