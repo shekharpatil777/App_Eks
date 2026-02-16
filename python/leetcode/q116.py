0122fe680e6367740b0910ff1b9512e705587dee
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-16 23:02:53 +0530

    Create q116.py

---
+++ python/leetcode/q116.py
@@ -0,0 +1,24 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None,
+                 right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        
+        leftmost = root
+        
+        while leftmost.left:
+            current = leftmost
+            
+            while current:
+                # Connect left -> right
+                current.left.next = current.right
+                
+
