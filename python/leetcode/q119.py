9171b52afb3194799970c87ee4c97cb30bcb5c13
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-19 21:42:19 +0530

    Update q119.py

--- python/leetcode/q119.py
+++ python/leetcode/q119.py
@@ -2,3 +2,9 @@ class Solution:
     def getRow(self, rowIndex: int) -> list[int]:
         row = [1]
         
+
+        for k in range(rowIndex):
+            next_val = row[-1] * (rowIndex - k) // (k + 1)
+            row.append(next_val)
+        
+        return row
