89b1450cd49c2eff6f0ec0e2be89cbdc5d8edb37
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-23 23:55:18 +0530

    Update q122.py

--- python/leetcode/q122.py
+++ python/leetcode/q122.py
@@ -5,3 +5,7 @@ class Solution:
         # Start from the second day
         for i in range(1, len(prices)):
             # If the price today is higher than yesterday, take the profit
+            if prices[i] > prices[i-1]:
+                total_profit += prices[i] - prices[i-1]
+                
+        return total_profit
