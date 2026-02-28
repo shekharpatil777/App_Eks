6f83025ad3be9b18d39b07b7e38f6eeeda1edab1
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-02-28 23:31:29 +0530

    Update q128.py

--- python/leetcode/q128.py
+++ python/leetcode/q128.py
@@ -10,3 +10,12 @@ def longestConsecutive(nums):
             current_num = num
             current_streak = 1
 
+
+            # Build the sequence upwards
+            while current_num + 1 in num_set:
+                current_num += 1
+                current_streak += 1
+
+            longest_streak = max(longest_streak, current_streak)
+
+    return longest_streak
