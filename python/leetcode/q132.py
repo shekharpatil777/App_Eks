2f4850fe98aa8996569aca781282707b2f09e2b7
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-06 23:54:42 +0530

    Create q132.py

---
+++ python/leetcode/q132.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partition(self, s: str) -> list[list[str]]:
+        res = []
+        path = []
+
+        def is_palindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(start):
+            # Base Case: If we've reached the end of the string, 
+            # the current path is a valid partition.
+            if start == len(s):
+                res.append(list(path))
+                return
+
+            # Explore all possible "cuts" from the current start position
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+                
