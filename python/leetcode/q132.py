1ef84e5550ced80979e35e0c935b03ed14891841
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-06 23:55:01 +0530

    Update q132.py

--- python/leetcode/q132.py
+++ python/leetcode/q132.py
@@ -17,3 +17,11 @@ class Solution:
             for end in range(start + 1, len(s) + 1):
                 substring = s[start:end]
                 
+
+                if is_palindrome(substring):
+                    path.append(substring)    # Choose
+                    backtrack(end)            # Explore
+                    path.pop()                # Un-choose (Backtrack)
+
+        backtrack(0)
+        return res
