5036da651d1e5fa6ad45f01a45b2f52b026164ef
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-09 23:38:21 +0530

    Update q135.py

--- python/leetcode/q135.py
+++ python/leetcode/q135.py
@@ -9,3 +9,11 @@ def canCompleteCircuit(gas, cost):
     for i in range(len(gas)):
         total_tank += gas[i] - cost[i]
         
+        # If we run out of gas at this station
+        if total_tank < 0:
+            # Pick the next station as the new starting point
+            start_index = i + 1
+            # Reset our current tank
+            total_tank = 0
+            
+    return start_index
