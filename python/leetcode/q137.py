09e777e7fce1c6e1551f85798e66c4f0f007195a
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-11 23:58:55 +0530

    Update q137.py

--- python/leetcode/q137.py
+++ python/leetcode/q137.py
@@ -3,3 +3,11 @@ class Solution:
         ones = 0
         twos = 0
         
+        for num in nums:
+            # Update 'ones' only if the bit isn't already in 'twos'
+            ones = (ones ^ num) & ~twos
+            
+            # Update 'twos' only if the bit isn't already in the new 'ones'
+            twos = (twos ^ num) & ~ones
+            
+        return ones
