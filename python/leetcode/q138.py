24555ccaf63440e57c28d1ed0d7652e73bc81711
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-12 23:47:39 +0530

    Update q138.py

--- python/leetcode/q138.py
+++ python/leetcode/q138.py
@@ -17,3 +17,14 @@ class Solution:
                 curr.next.random = curr.random.next
             curr = curr.next.next
             
+        # Step 3: Separate the two lists
+        curr = head
+        copy_head = head.next
+        while curr:
+            copy = curr.next
+            curr.next = copy.next
+            if copy.next:
+                copy.next = copy.next.next
+            curr = curr.next
+            
+        return copy_head
