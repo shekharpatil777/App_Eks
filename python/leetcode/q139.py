5b2a165b09cd66101b3a8fb91654624c4f76a6fc
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-13 23:25:08 +0530

    Update q139.py

--- python/leetcode/q139.py
+++ python/leetcode/q139.py
@@ -5,3 +5,11 @@ def wordBreak(s: str, wordDict: list[str]) -> bool:
     dp = [False] * (len(s) + 1)
     dp[0] = True
     
+    for i in range(1, len(s) + 1):
+        for j in range(i):
+            # If the prefix s[:j] is valid AND s[j:i] is in the dict
+            if dp[j] and s[j:i] in word_set:
+                dp[i] = True
+                break # Move to the next i once we find a valid split
+                
+    return dp[len(s)]
