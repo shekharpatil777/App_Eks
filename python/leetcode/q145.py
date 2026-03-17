39db6150badc11a7c0479379ea574f0d4bad0660
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-17 22:49:34 +0530

    Update q145.py

--- python/leetcode/q145.py
+++ python/leetcode/q145.py
@@ -9,3 +9,12 @@ class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
         
+        def traverse(node):
+            if not node:
+                return
+            traverse(node.left)    # Visit Left
+            traverse(node.right)   # Visit Right
+            result.append(node.val) # Visit Root
+            
+        traverse(root)
+        return result
