6be533a63f11cef5f09c8b194c6205470846da65
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-03-27 23:53:08 +0530

    Update q153.py

--- python/leetcode/q153.py
+++ python/leetcode/q153.py
@@ -5,3 +5,12 @@ class Solution:
         while left < right:
             mid = left + (right - left) // 2
             
+            # If mid element is greater than the far right element, 
+            # the min must be in the right half.
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            # Otherwise, the min is in the left half (including mid).
+            else:
+                right = mid
+                
+        return nums[left]
