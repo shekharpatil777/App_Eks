ee8909086d636ca52be025fe9e497fc7423c2a62
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-02 22:57:22 +0530

    Create q160.py

---
+++ python/leetcode/q160.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+
+        ptrA = headA
+        ptrB = headB
+
