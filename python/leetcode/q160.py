8ad34e3f4cd8b8ee60ec82306644f09c7a2247cc
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-02 22:57:34 +0530

    Update q160.py

--- python/leetcode/q160.py
+++ python/leetcode/q160.py
@@ -6,3 +6,11 @@ class Solution:
         ptrA = headA
         ptrB = headB
 
+        # Loop continues until the pointers meet
+        while ptrA != ptrB:
+            # If ptrA reaches the end, jump to headB, else move to next
+            ptrA = ptrA.next if ptrA else headB
+            # If ptrB reaches the end, jump to headA, else move to next
+            ptrB = ptrB.next if ptrB else headA
+            
+        return ptrA
