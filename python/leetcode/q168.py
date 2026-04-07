7e2a74fe1f8a6cb9be6eae7b1b34c543cbd46443
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-08 00:01:27 +0530

    Update q168.py

--- python/leetcode/q168.py
+++ python/leetcode/q168.py
@@ -8,3 +8,13 @@ class Solution:
             
             # Get the remainder (0-25)
             remainder = columnNumber % 26
+            
+            # Convert to character (ord('A') is 65)
+            char = chr(65 + remainder)
+            result.append(char)
+            
+            # Move to the next "digit"
+            columnNumber //= 26
+            
+        # The characters were added in reverse order (units first)
+        return "".join(reversed(result))
