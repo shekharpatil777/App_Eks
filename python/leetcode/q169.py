8a9c393e0b807179dee8e1ad8ff23f7974a8e56d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-08 23:51:13 +0530

    Update q169.py

--- python/leetcode/q169.py
+++ python/leetcode/q169.py
@@ -3,3 +3,12 @@ class Solution:
         candidate = None
         count = 0
 
+        for num in nums:
+            # If count is 0, we pick a new candidate
+            if count == 0:
+                candidate = num
+            
+            # If current num matches candidate, increment; else decrement
+            count += (1 if num == candidate else -1)
+
+        return candidate
