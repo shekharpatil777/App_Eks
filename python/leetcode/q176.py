d1285b03cc4b737a02c17e9dae205cdcc0d21f3d
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-13 23:56:40 +0530

    Create q176.py

---
+++ python/leetcode/q176.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    # 1. Get unique salaries and sort them descending
+    unique_salaries = employee['salary'].drop_duplicates().sort_values(ascending=False)
+    
+    # 2. Check if there are at least two unique salaries
+    if len(unique_salaries) < 2:
+        res = None
+    else:
+        # 3. Take the second highest (index 1)
+        res = unique_salaries.iloc[1]
