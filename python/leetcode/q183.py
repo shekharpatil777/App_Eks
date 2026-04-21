2985bcadb7cc3e01ceacae83e49244c7cecf77d0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-21 22:30:04 +0530

    Update q183.py

--- python/leetcode/q183.py
+++ python/leetcode/q183.py
@@ -3,3 +3,8 @@ import pandas as pd
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     # 1. Find customers whose 'id' is NOT in the 'customerId' column of orders
     df = customers[~customers['id'].isin(orders['customerId'])]
+    
+    # 2. Select only the 'name' column and rename it to 'Customers' as required
+    df = df[['name']].rename(columns={'name': 'Customers'})
+    
+    return df
