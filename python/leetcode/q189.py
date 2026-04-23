fc48d9d09f84cbb92dc66ab9a57e5bfdc3bc2a0f
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-04-23 21:22:35 +0530

    Create q189.py

---
+++ python/leetcode/q189.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n  # Handle cases where k > n
+        
