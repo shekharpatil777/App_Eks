8bcbb46ffa25ee1272fe23b04892779375949890
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-01 22:18:01 +0530

    Update q197.py

--- python/leetcode/q197.py
+++ python/leetcode/q197.py
@@ -3,3 +3,7 @@ import pandas as pd
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     # 1. Sort by id in ascending order to keep the smallest id
     person.sort_values(by='id', ascending=True, inplace=True)
+    
+    # 2. Drop duplicates based on email, keeping the first occurrence
+    # 'inplace=True' modifies the original dataframe as required by the problem
+    person.drop_duplicates(subset='email', keep='first', inplace=True)
