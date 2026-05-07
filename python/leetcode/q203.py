7f167d9734523c85f5b72591346777a18fcbddc8
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-07 23:35:50 +0530

    Create q203.py

---
+++ python/leetcode/q203.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # Create a dummy node to act as the new head's predecessor
+        dummy = ListNode(next=head)
+        prev, curr = dummy, head
