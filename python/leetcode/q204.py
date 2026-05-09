c21c8703c859621054c04989ebe4d643cd2502b2
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-09 23:22:04 +0530

    Update q204.py

--- python/leetcode/q204.py
+++ python/leetcode/q204.py
@@ -7,3 +7,12 @@ class Solution:
         primes = [True] * n
         primes[0] = primes[1] = False
 )
+        
+        # Only need to check up to the square root of n
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                # Slicing is much faster in Python for marking multiples
+                # Start at i*i, go to n, step by i
+                primes[i*i:n:i] = [False] * len(primes[i*i:n:i])
+        
+        return sum(primes
