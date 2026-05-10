f278d021f660a71d7630810ac2515ba6f9b179f3
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-10 23:45:06 +0530

    Create q205.py

---
+++ python/leetcode/q205.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # If lengths differ, they cannot be isomorphic
+        if len(s) != len(t):
+            return False
+        
+        # Dictionaries to store the mappings
+        mapST, mapTS = {}, {}
+
+        for charS, charT in zip(s, t):
+            # Check if charS is already mapped to something else
+            if charS in mapST and mapST[charS] != charT:
+                return False
