01e197962fb3fc6c73ce54c7c8811023cb3d15dd
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-21 23:37:00 +0530

    Create q211.py

---
+++ python/leetcode/q211.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+
+            node = node.children[ch]
+
+        node.end = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(i, node):
+            if i == len(word):
+                return node.end
+
+            ch = word[i]
+
+            if ch == ".":
+                for child in node.children.values():
+                    if dfs(i + 1, child):
+                        return True
+                return False
+)
