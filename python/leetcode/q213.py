e50cd43fca0a5522b441b6a4ecde38164372d88b
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-23 23:57:05 +0530

    Update q213.py

--- python/leetcode/q213.py
+++ python/leetcode/q213.py
@@ -10,3 +10,13 @@ class Solution:
         # Helper function from House Robber I (linear layout)
         def rob_linear(houses: list[int]) -> int:
             rob1, rob2 = 0, 0
+            
+            # Dynamic programming: track max money up to current house
+            for n in houses:
+                temp = max(n + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+            return rob2
+
+        # The max of skipping the last house OR skipping the first house
+        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
