76ab08b43b14b600e3edb4626612bd3a18477817
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-25 22:56:45 +0530

    Update q123.py

--- python/leetcode/q123.py
+++ python/leetcode/q123.py
@@ -5,3 +5,10 @@ class Solution:
         buy2 = float('-inf')
         sell2 = 0
 
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1 + price)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, buy2 + price)
+
+        return sell2
