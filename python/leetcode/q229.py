ad2bea8243a0179973ef1b6d9e713fd8760c9230
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-05-31 21:49:25 +0530

    Create q229.py

---
+++ python/leetcode/q229.py
@@ -0,0 +1,24 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        cand1 = cand2 = None
+        count1 = count2 = 0
+
+        for num in nums:
+            if num == cand1:
+                count1 += 1
+            elif num == cand2:
+                count2 += 1
+            elif count1 == 0:
+                cand1 = num
+                count1 = 1
+            elif count2 == 0:
+                cand2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        result = []
+
+        if nums.count(cand1) > len(nums) // 3:
+            result.append(cand1)
