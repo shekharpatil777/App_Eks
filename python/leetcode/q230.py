f9e0af1c925034c84fa31905868aeae8afbd2982
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-06-01 22:43:04 +0530

    Create q230.py

---
+++ python/leetcode/q230.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+        
+        while curr or stack:
+            # Travel to the leftmost node
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            # Process the node
+            curr = stack.pop()
+            k -= 1
