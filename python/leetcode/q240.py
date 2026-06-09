fc602a247b8e31e9096197adb589dbd99bd052c9
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-06-09 23:29:43 +0530

    Create q240.py

---
+++ python/leetcode/q240.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        rows, cols = len(matrix), len(matrix[0])
+        r, c = 0, cols - 1
