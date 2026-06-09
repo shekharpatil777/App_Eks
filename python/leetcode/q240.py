0dd3f8cf0248c177f276e421bcc31991a37cfe18
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-06-09 23:29:59 +0530

    Update q240.py

--- python/leetcode/q240.py
+++ python/leetcode/q240.py
@@ -7,3 +7,13 @@ class Solution:
 
         rows, cols = len(matrix), len(matrix[0])
         r, c = 0, cols - 1
+
+        while r < rows and c >= 0:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                c -= 1
+            else:
+                r += 1
+
+        return False
