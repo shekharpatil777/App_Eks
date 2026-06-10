37c148bf0a976c69dc94ba2333d17d1f91c8e1cb
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-06-10 23:20:05 +0530

    Create q241.py

---
+++ python/leetcode/q241.py
@@ -0,0 +1,23 @@
+from typing import List
+from functools import lru_cache
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+
+        @lru_cache(None)
+        def solve(expr):
+            result = []
+
+            for i, ch in enumerate(expr):
+                if ch in "+-*":
+                    left = solve(expr[:i])
+                    right = solve(expr[i + 1:])
+
+                    for l in left:
+                        for r in right:
+                            if ch == '+':
+                                result.append(l + r)
+                            elif ch == '-':
+                                result.append(l - r)
+                            else:
+                                result.append(l * r)
