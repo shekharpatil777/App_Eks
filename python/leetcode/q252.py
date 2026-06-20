595483dd181797281eb153f307858f196dcce983
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-06-20 23:33:11 +0530

    Update q252.py

--- python/leetcode/q252.py
+++ python/leetcode/q252.py
@@ -1,3 +1,9 @@
 class Solution:
     def canAttendMeetings(self, intervals):
         intervals.sort(key=lambda x: x[0])
+
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < intervals[i - 1][1]:
+                return False
+
+        return True
