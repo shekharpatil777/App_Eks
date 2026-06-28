1382a70c1e20d4837f6682f7cd28f9f8adc8e265
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-06-28 23:54:49 +0530

    Update q260.py

--- python/leetcode/q260.py
+++ python/leetcode/q260.py
@@ -11,3 +11,11 @@ class Solution:
 
         a = 0
         b = 0
+
+        for num in nums:
+            if num & diff:
+                a ^= num
+            else:
+                b ^= num
+
+        return [a, b]
