992736fd35f337e7058034c318bdb620b95600eb
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-07 22:51:28 +0530

    Update q276.py

--- python/leetcode/q276.py
+++ python/leetcode/q276.py
@@ -7,3 +7,12 @@ class Solution:
         
         same = k          # first 2 posts same color
         diff = k * (k - 1)  # first 2 posts different color
+        
+        for i in range(3, n + 1):
+            new_same = diff
+            new_diff = (same + diff) * (k - 1)
+            
+            same = new_same
+            diff = new_diff
+        
+        return same + diff
