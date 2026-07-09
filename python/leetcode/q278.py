b643646f92ced802afadbc11333e7aa79d137ac8
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-09 23:05:00 +0530

    Update q278.py

--- python/leetcode/q278.py
+++ python/leetcode/q278.py
@@ -7,3 +7,10 @@ class Solution:
 
         while left < right:
             mid = left + (right - left) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
