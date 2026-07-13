307c4938c62ea21fc81e23008ce2ab9b515704aa
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-13 23:35:37 +0530

    Create q282.py

---
+++ python/leetcode/q282.py
@@ -0,0 +1,51 @@
+from typing import List
+
+
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        result = []
+
+        def backtrack(
+            index: int,
+            expression: str,
+            value: int,
+            previous: int
+        ) -> None:
+            # All digits have been used
+            if index == len(num):
+                if value == target:
+                    result.append(expression)
+                return
+
+            for end in range(index, len(num)):
+                # Numbers such as "05" are not allowed
+                if end > index and num[index] == "0":
+                    break
+
+                current_str = num[index:end + 1]
+                current = int(current_str)
+
+                # First number: no operator before it
+                if index == 0:
+                    backtrack(
+                        end + 1,
+                        current_str,
+                        current,
+                        current
+                    )
+                else:
+                    # Addition
+                    backtrack(
+                        end + 1,
+                        expression + "+" + current_str,
+                        value + current,
+                        current
+                    )
+
+                    # Subtraction
+                    backtrack(
+                        end + 1,
+                        expression + "-" + current_str,
+                        value - current,
+                        -current
+                    )
