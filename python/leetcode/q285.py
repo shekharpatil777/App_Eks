6762f5efea041101e752d40b7ccd0e00f2f25d82
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-14 23:26:29 +0530

    Update q285.py

--- python/leetcode/q285.py
+++ python/leetcode/q285.py
@@ -8,3 +8,12 @@
 class Solution:
     def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
         successor = None
+
+        while root:
+            if p.val < root.val:
+                successor = root
+                root = root.left
+            else:
+                root = root.right
+
+        return successor
