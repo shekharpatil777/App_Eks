5f2ba96ef7dbc5be8d518748d4e4e8bb36d5426a
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-16 23:15:10 +0530

    Update q287.py

--- python/leetcode/q287.py
+++ python/leetcode/q287.py
@@ -9,3 +9,11 @@ class Solution:
             fast = nums[nums[fast]]
             if slow == fast:
                 break
+
+        # Phase 2: Find entrance to cycle
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
