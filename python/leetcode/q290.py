e871c2980e090af17814e273c30c9ce1e4bd15b7
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-18 21:30:19 +0530

    Create q290.py

---
+++ python/leetcode/q290.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for ch, word in zip(pattern, words):
