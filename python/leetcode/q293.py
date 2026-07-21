0736a947107c1d8d2df2f20a1bd516447dad1b8b
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-21 23:28:07 +0530

    Update q293.py

--- python/leetcode/q293.py
+++ python/leetcode/q293.py
@@ -4,3 +4,14 @@ from typing import List
 class Solution:
     def generatePossibleNextMoves(self, currentState: str) -> List[str]:
         result = []
+
+        for i in range(len(currentState) - 1):
+            if currentState[i:i + 2] == "++":
+                next_state = (
+                    currentState[:i]
+                    + "--"
+                    + currentState[i + 2:]
+                )
+                result.append(next_state)
+
+        return result
