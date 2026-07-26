8cbb80a7114ac812c8556e9b911f3b86ac20ad67
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-26 23:10:50 +0530

    Create q299.py

---
+++ python/leetcode/q299.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        bulls = 0
+        secret_count = [0] * 10
+        guess_count = [0] * 10
+
+        for s, g in zip(secret, guess):
+            if s == g:
+                bulls += 1
+            else:
+                secret_count[int(s)] += 1
+                guess_count[int(g)] += 1
+"
