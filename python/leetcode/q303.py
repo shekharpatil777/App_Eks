71bf60e6ff0f7faf0eac761833309f03368c91e6
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-07-31 23:25:31 +0530

    Update q303.py

--- python/leetcode/q303.py
+++ python/leetcode/q303.py
@@ -8,3 +8,6 @@ class NumArray:
 
         for num in nums:
             self.prefix_sum.append(self.prefix_sum[-1] + num)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix_sum[right + 1] - self.prefix_sum[left]
