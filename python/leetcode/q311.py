bd7f75a7384a99038211fd02e176df6c6fc58b01
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-08-06 22:43:03 +0530

    Update q311.py

--- python/leetcode/q311.py
+++ python/leetcode/q311.py
@@ -12,3 +12,14 @@ class Solution:
         cols = len(mat2[0])
 
         result = [[0] * cols for _ in range(rows)]
+
+        for i in range(rows):
+            for k in range(common):
+                if mat1[i][k] == 0:
+                    continue
+
+                for j in range(cols):
+                    if mat2[k][j] != 0:
+                        result[i][j] += mat1[i][k] * mat2[k][j]
+
+        return result
