244d8acfaf1682434f9840a801ca3ba016e97a8f
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-08-08 23:24:43 +0530

    Update q313.py

--- python/leetcode/q313.py
+++ python/leetcode/q313.py
@@ -7,3 +7,14 @@ class Solution:
         ugly = [1] * n
         pointers = [0] * k
         values = primes[:]
+
+        for i in range(1, n):
+            next_ugly = min(values)
+            ugly[i] = next_ugly
+
+            for j in range(k):
+                if values[j] == next_ugly:
+                    pointers[j] += 1
+                    values[j] = primes[j] * ugly[pointers[j]]
+
+        return ugly[-1]
