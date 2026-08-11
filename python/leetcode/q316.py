99715a941d68bf5d05180f1752f13aec41995187
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-08-11 23:37:08 +0530

    Update q316.py

--- python/leetcode/q316.py
+++ python/leetcode/q316.py
@@ -8,3 +8,16 @@ class Solution:
         for i, ch in enumerate(s):
             if ch in seen:
                 continue
+
+            while (
+                stack
+                and ch < stack[-1]
+                and last[stack[-1]] > i
+            ):
+                removed = stack.pop()
+                seen.remove(removed)
+
+            stack.append(ch)
+            seen.add(ch)
+
+        return "".join(stack)
