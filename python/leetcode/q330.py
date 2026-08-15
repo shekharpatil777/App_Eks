f301ff300f6bec58983a81cb62e731faca0af484
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-08-15 22:26:17 +0530

    Update q330.py

--- python/leetcode/q330.py
+++ python/leetcode/q330.py
@@ -5,3 +5,13 @@ class Solution:
         miss = 1
         i = 0
         patches = 0
+
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+
+        return patches
