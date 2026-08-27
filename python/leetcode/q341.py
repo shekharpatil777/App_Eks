4ea4db78034154081f9f09a29ffbe4ff9215710b
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-08-27 23:46:36 +0530

    Update q341.py

--- python/leetcode/q341.py
+++ python/leetcode/q341.py
@@ -4,3 +4,15 @@ class NestedIterator:
 
     def next(self) -> int:
         return self.stack.pop().getInteger()
+
+    def hasNext(self) -> bool:
+        while self.stack:
+            top = self.stack[-1]
+
+            if top.isInteger():
+                return True
+
+            self.stack.pop()
+            self.stack.extend(top.getList()[::-1])
+
+        return False
