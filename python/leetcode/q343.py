26040ae8d2fddc3355fa4988b9ad2a6fa9187d92
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-02 23:53:56 +0530

    Update q343.py

--- python/leetcode/q343.py
+++ python/leetcode/q343.py
@@ -5,3 +5,14 @@ class Solution:
             return 1
         if n == 3:
             return 2
+
+        # Count how many 3s can be formed
+        quotient, remainder = divmod(n, 3)
+
+        if remainder == 0:
+            return 3**quotient
+        elif remainder == 1:
+            # 3 + 1 is better split as 2 * 2
+            return (3 ** (quotient - 1)) * 4
+        else:  # remainder == 2
+            return (3**quotient) * 2
