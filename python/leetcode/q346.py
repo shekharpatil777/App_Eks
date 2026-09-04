d4120b26e0ca53efb17b20a1ef0b25a774206398
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-04 23:26:50 +0530

    Update q346.py

--- python/leetcode/q346.py
+++ python/leetcode/q346.py
@@ -6,3 +6,12 @@ class MovingAverage:
         self.size = size
         self.q = deque()
         self.total = 0
+
+    def next(self, val: int) -> float:
+        self.q.append(val)
+        self.total += val
+
+        if len(self.q) > self.size:
+            self.total -= self.q.popleft()
+
+        return self.total / len(self.q)
