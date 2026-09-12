7ab7f4564ae69d3eef854b5b70ab90062393d2f7
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-12 23:44:42 +0530

    Create q354.py

---
+++ python/leetcode/q354.py
@@ -0,0 +1,15 @@
+import bisect
+from typing import List
+
+
+class Solution:
+
+  def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+    if not envelopes:
+      return 0
+
+    # Sort: width ascending, height descending on tie
+    envelopes.sort(key=lambda x: (x[0], -x[1]))
+
+    # Longest Increasing Subsequence on heights
+    lis = []
