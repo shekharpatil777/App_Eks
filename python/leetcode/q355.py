d3f45bcf1e427335e8a57ec270ba4b25af25beb0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-14 23:40:28 +0530

    Create q355.py

---
+++ python/leetcode/q355.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+import heapq
+
+class Twitter:
+
+    def __init__(self):
+        # Global timestamp to order tweets chronologically
+        self.time = 0
+        # Maps userId to a list of (timestamp, tweetId) tuples
+        self.tweet_map = defaultdict(list)
+        # Maps userId to a set of followeeIds
+        self.follow_map = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweet_map[userId].append((self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> list[int]:
+        res = []
+        # Max-heap elements: (-timestamp, tweetId, followeeId, next_index_in_tweets)
+        max_heap = []
+        
+        # User always implicitly follows themselves
+        followees = self.follow_map[userId] | {userId}
+
+        # Initialize heap with the most recent tweet from each followee
+        for followee_id in followees:
+            tweets = self.tweet_map[followee_id]
+            if tweets:
+                last_idx = len(tweets) - 1
+                time, tweet_id = tweets[last_idx]
+                heapq.heappush(max_heap, (-time, tweet_id, followee_id, last_idx - 1))
+
+        # Extract up to 10 most recent tweets
+        while max_heap and len(res) < 10:
+            neg_time, tweet_id, followee_id, next_idx = heapq.heappop(max_heap)
+            res.append(tweet_id)
+            
+            # Push the next recent tweet from the same user if available
+            if next_idx >= 0:
+                time, next_tweet_id = self.tweet_map[followee_id][next_idx]
+                heapq.heappush(max_heap, (-time, next_tweet_id, followee_id, next_idx - 1))
+
+        return res
