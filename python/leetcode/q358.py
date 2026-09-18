8ff7440a456d0863bdf667b41ca732abbb3ad362
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-18 23:25:04 +0530

    Create q358.py

---
+++ python/leetcode/q358.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortTransformedArray(self, nums, a, b, c):
+        def f(x):
+            return a * x * x + b * x + c
+
+        n = len(nums)
+        ans = [0] * n
+
+        left, right = 0, n - 1
+
+        # Parabola opens upward → largest values at the ends
+        if a > 0:
+            pos = n - 1
+
+            while left <= right:
+                l = f(nums[left])
+                r = f(nums[right])
+
+                if l > r:
+                    ans[pos] = l
+                    left += 1
+                else:
+                    ans[pos] = r
+                    right -= 1
+
+                pos -= 1
+
+        # Parabola opens downward → smallest values at the ends
+        else:
+            pos = 0
+
+            while left <= right:
+                l = f(nums[left])
+                r = f(nums[right])
