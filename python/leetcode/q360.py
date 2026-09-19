42b62757ee693f6865b35a75de7117e788910eb0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-19 23:41:58 +0530

    Create q360.py

---
+++ python/leetcode/q360.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        def f(x):
+            return a * x * x + b * x + c
+
+        n = len(nums)
+        ans = [0] * n
+
+        left, right = 0, n - 1
+
+        if a >= 0:
+            # Largest values come from the ends
+            pos = n - 1
+
+            while left <= right:
+                l = f(nums[left])
+                r = f(nums[right])
+
+                if l > r:
+                    ans[pos] = l
+                    left += 1
+                else:
+                    ans[pos] = r
+                    right -= 1
+
+                pos -= 1
+        else:
+            # Smallest values come from the ends
+            pos = 0
