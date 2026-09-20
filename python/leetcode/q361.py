2fa2ee7912705eae0900952e532139dae012eafe
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2026-09-20 23:41:59 +0530

    Create q361.py

---
+++ python/leetcode/q361.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxKilledEnemies(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        col_hits = [0] * n
+
+        for i in range(m):
+            row_hits = 0
+
+            for j in range(n):
+                # Recalculate enemies in this row segment
+                if j == 0 or grid[i][j - 1] == 'W':
+                    row_hits = 0
+                    k = j
+                    while k < n and grid[i][k] != 'W':
+                        if grid[i][k] == 'E':
+                            row_hits += 1
+                        k += 1
+
+                # Recalculate enemies in this column segment
+                if i == 0 or grid[i - 1][j] == 'W':
+                    col_hits[j] = 0
